f5014fb7188514185a4df2bf6b085c6e58ddabee
Pamela Arteaga Campos <pamelaarteaga@MacBook-Pro-de-Pamela.local>
2023-07-01 13:36:52 -0400

    fix app.py

--- server/app.py
+++ server/app.py
@@ -10,10 +10,7 @@ socketio = SocketIO(app, cors_allowed_origins='*')
 def handle_message(message):
     print('received message: ' + message)
     answer = ask(message)
-<<<<<<< HEAD
     send(message, broadcast=True)
-=======
->>>>>>> 3c19aad8ca4515ed09b4ff126685875252d26351
     send(answer, broadcast=True)
 
 if __name__ == '__main__':
