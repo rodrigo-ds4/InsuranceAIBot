56d361032e70e330da4fb130b41fb4ea08a58eee
Pamela Arteaga Campos <pamelaarteaga@MacBook-Pro-de-Pamela.local>
2023-07-02 20:43:20 -0400

    add loader to chatbot and options as first interaction

--- client/chatbot/src/App.js
+++ client/chatbot/src/App.js
@@ -4,11 +4,11 @@ import ChatView from "./components/ChatView/ChatView";
 let endpoint = "http://localhost:5000";
 let socket = io.connect(endpoint);
 
-
 const App = () => {
   const [newQuestion, sendNewQuestion] = useState("");
   const [chatAnswers, saveChatAnswer] = useState([]);
   const [userQuestions, saveUserQuestions] = useState([]);
+  const [showOptions, setShowOptions] = useState(true);
 
   socket.on("message", (msg) => {
     saveChatAnswer([...chatAnswers, msg]);
@@ -21,20 +21,60 @@ const App = () => {
   const sendQuestion = () => {
     if (newQuestion !== "") {
       socket.emit("message", newQuestion);
-    sendNewQuestion("");
-    saveUserQuestions([...userQuestions, newQuestion]);
-
+      sendNewQuestion("");
+      saveUserQuestions([...userQuestions, newQuestion]);
     }
   };
+
+  const selectedOption = (name, code) => {
+    console.log("selected option", code);
+    setShowOptions(false);
+    saveUserQuestions([...userQuestions, name]);
+    socket.emit("message", name);
+  };
+
+  const insuranceOptions = [
+    {
+      name: "Accidentes Personales / Reembolso Gastos Médicos ",
+      code: ["POL120190177"],
+    },
+    {
+      name: "Seguro Colectivo Complementario de Salud ",
+      code: ["POL320130223"],
+    },
+    {
+      name: "Seguro para Prestaciones Médicas Derivadas de Accidente y Enfermedad",
+      code: ["POL320150503"],
+    },
+    {
+      name: "Seguro para Prestaciones Médicas Derivadas de Hospitalización Quirúrgica de Emergencia",
+      code: ["POL320180100"],
+    },
+    {
+      name: "Seguro para Prestaciones Médicas de Alto Costo",
+      code: ["POL320190074", "POL320200214", "POL320210210"],
+    },
+    {
+      name: "Seguro Individual Catastrófico por Evento",
+      code: ["POL320200071"],
+    },
+    {
+      name: "Seguro Individual Obligatorio de Salud Asociado a Covid 19",
+      code: ["POL320210063"],
+    },
+  ];
 
   return (
     <div>
       <ChatView
+        options={insuranceOptions}
         questions={userQuestions}
         answers={chatAnswers}
         newQuestion={newQuestion}
         handleChange={(e) => inputQuestion(e)}
         handleClick={() => sendQuestion()}
+        handleOption={(name, code) => selectedOption(name, code)}
+        disabled={showOptions}
       />
     </div>
   );
--- client/chatbot/src/components/ChatView/ChatView.jsx
+++ client/chatbot/src/components/ChatView/ChatView.jsx
@@ -3,11 +3,14 @@ import ChatBubble from "../ChatBubble/ChatBubble";
 import styles from "./styles.module.scss";
 
 const ChatView = ({
+  options,
   questions,
   answers,
   newQuestion,
   handleChange,
   handleClick,
+  handleOption,
+  disabled,
 }) => {
   const chatWindowRef = useRef(null);
 
@@ -34,6 +37,13 @@ const ChatView = ({
               "Hola! Soy Nicolle, la asistente virtual de Seguros. ¿En qué te puedo ayudar?"
             }
           />
+          {disabled && (
+            <div className={styles.chat_options}>
+              {options.map((option, i) => (
+                <button className={styles.chat_options_button} key={`option${i}`} onClick={() => handleOption(option.name, option.code)}>{option.name}</button>
+              ))}
+            </div>
+          )}
           {questions.map((question, i) => (
             <div key={`answer${i}`}>
               <ChatBubble role="user" content={question} />
@@ -46,8 +56,14 @@ const ChatView = ({
             type="text"
             onChange={(e) => handleChange(e)}
             value={newQuestion}
+            disabled={disabled ? "disabled" : ""}
           />
-          <input type="button" onClick={handleClick} value="⌲" />
+          <input
+            type="button"
+            onClick={handleClick}
+            value="⌲"
+            disabled={disabled ? "disabled" : ""}
+          />
         </div>
       </div>
     </div>
