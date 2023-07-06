96be5c86e154dfb8c431c7e8f0798cb09984da77
Rodrigo de Sarasqueta <78835285+rotpando@users.noreply.github.com>
2023-07-06 07:16:03 -0300

    memory test

--- server/answer.py
+++ server/answer.py
@@ -1,28 +1,40 @@
 from dotenv import load_dotenv
 import openai
 import os
-from langchain.document_loaders import TextLoader, PyPDFLoader
-from langchain.text_splitter import CharacterTextSplitter, TokenTextSplitter
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 
-from langchain.schema import Document
-
 from langchain.indexes import VectorstoreIndexCreator
+from langchain.memory import ChatMessageHistory
 
 load_dotenv()
+history = ChatMessageHistory()
+
 current_directory = os.getcwd()
 print("Directorio actual:", current_directory)
 openai.api_key = os.environ['OPENAI_API_KEY']
 print(os.environ['OPENAI_API_KEY'])
 
-def ask(question, code):
+def ask(question, code, last_q, last_a):
     embedding = OpenAIEmbeddings()
     print("code: " + code)
+
+    #history_text = "Historial de chat: \n"
+    #for chat in chat_history:
+    #    history_text += "pregunta: " + chat["question"] + "\n"
+        #history_text += "respuesta: " + chat["answer"] + "\n"
+
     loader = PyPDFLoader("raw-dataset/" + code + ".pdf")
 
     index = VectorstoreIndexCreator(
-        text_splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=20),
-        embedding = OpenAIEmbeddings()
+        #text_splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=0),
+        #embedding = embedding
     ).from_loaders([loader])
-    generated_text = index.query(question)
-    return generated_text
+    history.add_user_message(question)
+    question = "Mi nombre es Nicolle, bot asistente de seguros. Historial: Pregunta anterior del usuario fue: " + last_q + ". Respuesta: " + last_a +". Pregunta nueva del usuario:" + question
+    print(question)
+    answer = index.query(question)
+    history.add_ai_message(answer)
+
+    return answer
--- server/app.py
+++ server/app.py
@@ -6,11 +6,13 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'prueba' 
 socketio = SocketIO(app, cors_allowed_origins='*')
 code = ""
+last_question = ""
+last_answer = ""
 
 @socketio.on('message')
 def handle_message(message):
     print('received message: ' + message)
-    answer = ask(message, code)
+    answer = ask(message, code, last_question, last_answer)
     send(answer, broadcast=True)
 
 @socketio.on('action')
@@ -18,14 +20,18 @@ def handle_message(action):
     print('action: ' + action)
     if action[:3] == "POL":
         global code
+        global last_question
+        global last_answer
         code = action
+        last_question = ""
+        last_answer = ""
         send("Conversemos sobra la póliza " + action[3:] + ", qué te gustaría saber?", broadcast=True)
     elif action[:3] == "NEW":
         send("Generemos una nueva poliza. Indicame que requerimientos tienes? ", broadcast=True)
     elif action[:3] == "FND":
         send("Voy a buscar polizas en Google, indicame que tipo de poliza buscas.", broadcast=True)
     else:
-        print("action " + action + " is not avaliable.")
+        print("action " + action + " is not available.")
         send("error", broadcast=True)
     #answer = ask(message)
 
